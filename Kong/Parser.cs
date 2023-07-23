d5a8b2a50a41c17d220e486138ac898450d4f76a
xafero <georg.berendt@outlook.com>
2023-07-23 02:00:27 +0200

    Adding easier parsing

---
+++ Kong/Parser.cs
@@ -0,0 +1,35 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Kong
+{
+	public static class Parser
+	{
+		public static Parsed Parse(string text)
+		{
+			var browser = new Browser(text);
+			return new Parsed(browser);
+		}
+
+		public enum OS
+		{
+			Unknown = 0
+		}
+
+		public sealed class Parsed
+		{
+			private readonly Browser _browser;
+
+			public Parsed(Browser browser)
+			{
+				_browser = browser;
+			}
+
+			public string Name => _browser.Name;
+			public string Version => _browser.Version;
+
+			public OS OS
+			{
+				get { return OS.Unknown; }
+			}
+		}
+	}
+}
