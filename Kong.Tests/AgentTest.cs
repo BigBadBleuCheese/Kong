6e3273b7967cb136e2364233936a3138eccab330
xafero <georg.berendt@outlook.com>
2023-07-23 03:03:15 +0200

    Adding a lot of tests

--- Kong.Tests/AgentTest.cs
+++ Kong.Tests/AgentTest.cs
@@ -5,44 +5,46 @@ namespace Kong.Tests
 	public class AgentTest
 	{
 		[Theory]
-		[InlineData("Microsoft Edge", "14.14393", "",
+		[InlineData("Microsoft Edge", "14.14393", "10", "Windows MsEdge Gecko Unknown Unknown",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393")]
-		[InlineData("Chrome", "55.0", "",
+		[InlineData("Chrome", "55.0", "10", "Windows Chrome Blink Unknown Unknown",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")]
-		[InlineData("Safari", "9.0", "",
+		[InlineData("Safari", "9.0", "9.1", "iOS Safari Webkit iPhone Mobile",
 			"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1")]
-		[InlineData("Safari", "9.0", "",
+		[InlineData("Safari", "9.0", "9.1", "iOS Safari Webkit iPad Tablet",
 			"Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1")]
-		[InlineData("Chrome", "55.0", "",
+		[InlineData("Chrome", "55.0", "5.1.1", "Android Chrome Blink Unknown Mobile",
 			"Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36")]
-		[InlineData("Chrome", "55.0", "",
+		[InlineData("Chrome", "55.0", "5.0", "Android Chrome Blink Unknown Mobile",
 			"Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36")]
-		[InlineData("Chrome", "114.0", "",
+		[InlineData("Chrome", "114.0", "10", "Windows Chrome Blink Unknown Unknown",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")]
-		[InlineData("Firefox", "115.0", "",
+		[InlineData("Firefox", "115.0", "10", "Windows Firefox Gecko Unknown Unknown",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0")]
-		[InlineData("Chrome", "114.0", "",
+		[InlineData("Chrome", "114.0", null, "Linux Chrome Blink Unknown Unknown",
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")]
-		[InlineData("Firefox", "115.0", "",
+		[InlineData("Firefox", "115.0", null, "Linux Firefox Gecko Unknown Unknown",
 			"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0")]
-		[InlineData("Opera", "100.0", "",
+		[InlineData("Opera", "100.0", null, "Linux Opera Blink Unknown Unknown",
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 OPR/100.0.0.0")]
-		[InlineData("Microsoft Edge", "114.0", "",
+		[InlineData("Microsoft Edge", "114.0", null, "Linux MsEdge Gecko Unknown Unknown",
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82")]
-		[InlineData("Chrome", "114.0", "",
+		[InlineData("Chrome", "114.0", "10", "Android Chrome Blink Unknown Mobile",
 			"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36")]
-		[InlineData("Chrome", "114.0", "",
+		[InlineData("Chrome", "114.0", "10", "Android Chrome Blink Unknown Mobile",
 			"Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36")]
-		[InlineData("Firefox", "115.0", "",
+		[InlineData("Firefox", "115.0", null, "Linux Firefox Gecko Unknown Unknown",
 			"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0")]
-		[InlineData("Firefox", "115.0", "",
+		[InlineData("Firefox", "115.0", "10", "Android Firefox Gecko Unknown Mobile",
 			"Mozilla/5.0 (Android 10; Mobile; rv:109.0) Gecko/115.0 Firefox/115.0")]
-		public void CheckUserAgent(string name, string ver, string os, string text)
+		public void CheckUserAgent(string name, string ver, string ov, string os, string text)
 		{
 			var kong = Parser.Parse(text);
 			Assert.Equal(name, kong.Name);
 			Assert.Equal(ver, kong.Version);
-			Assert.Equal(os, kong.OS.ToString());
+			Assert.Equal(ov, kong.OSVersion);
+			var tmp = $"{kong.OS} {kong.Base} {kong.Engine} {kong.Model} {kong.Trait}";
+			Assert.Equal(os, tmp);
 		}
 	}
 }
--- Kong/Parser.cs
+++ Kong/Parser.cs
@@ -1,4 +1,6 @@
 ﻿// ReSharper disable InconsistentNaming
+using System;
+
 namespace Kong
 {
 	public static class Parser
@@ -11,7 +13,44 @@ namespace Kong
 
 		public enum OS
 		{
-			Unknown = 0
+			Unknown = 0,
+			Android,
+			iOS,
+			Linux,
+			Windows
+		}
+
+		public enum Engine
+		{
+			Unknown = 0,
+			Blink,
+			Gecko,
+			Webkit
+		}
+
+		public enum Model
+		{
+			Unknown = 0,
+			iPhone,
+			iPad
+		}
+
+		[Flags]
+		public enum Trait
+		{
+			Unknown = 0,
+			Mobile = 1,
+			Tablet = 2
+		}
+
+		public enum Base
+		{
+			Unknown = 0,
+			Chrome,
+			Firefox,
+			MsEdge,
+			Opera,
+			Safari
 		}
 
 		public sealed class Parsed
@@ -25,10 +64,63 @@ namespace Kong
 
 			public string Name => _browser.Name;
 			public string Version => _browser.Version;
+			public string OSVersion => _browser.OSVersion;
+
+			public Engine Engine
+			{
+				get
+				{
+					if (_browser.Blink) return Engine.Blink;
+					if (_browser.Gecko) return Engine.Gecko;
+					if (_browser.Webkit) return Engine.Webkit;
+					return Engine.Unknown;
+				}
+			}
+
+			public Model Model
+			{
+				get
+				{
+					if (_browser.iPhone) return Model.iPhone;
+					if (_browser.iPad) return Model.iPad;
+					return Model.Unknown;
+				}
+			}
+
+			public Trait Trait
+			{
+				get
+				{
+					var current = Trait.Unknown;
+					if (_browser.Mobile) current |= Trait.Mobile;
+					if (_browser.Tablet) current |= Trait.Tablet;
+					return current;
+				}
+			}
+
+			public Base Base
+			{
+				get
+				{
+					if (_browser.Chrome) return Base.Chrome;
+					if (_browser.Firefox) return Base.Firefox;
+					if (_browser.MsEdge) return Base.MsEdge;
+					if (_browser.Opera) return Base.Opera;
+					if (_browser.Safari) return Base.Safari;
+					return Base.Unknown;
+				}
+			}
 
 			public OS OS
 			{
-				get { return OS.Unknown; }
+				get
+				{
+					if (_browser.Android) return OS.Android;
+					if (_browser.iOS) return OS.iOS;
+					if (_browser.Linux) return OS.Linux;
+					if (_browser.Windows) return OS.Windows;
+					return OS.Unknown;
+				}
 			}
 		}
 	}
